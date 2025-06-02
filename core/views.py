f07e9721b6542c5ad494aef2b3ef123a441cdfd3
Vanessa-teles <soymichefa001@gmail.com>
2025-06-02 11:31:07 -0300

    testnovo

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.urls import reverse
 from .forms import ContatoForm
 
 def index(request):
@@ -10,16 +11,11 @@ def contato(request):
         form = ContatoForm(request.POST)
         if form.is_valid():
             try:
-                form.sendEmail()
+                form.send_mail()
                 messages.success(request, 'E-mail enviado com sucesso!')
-                return redirect('contato')
+                return redirect(reverse('contato'))
             except Exception as e:
-                messages.error(request, f'Erro ao enviar e-mail: {e}')
-                return redirect('contato')
-    else:
-        form = ContatoForm()
-    
-    context = {
-        'form': form
-    }
-    return render(request, 'core/contato.html', context)
+                messages.error(request, f'Erro ao enviar e-mail: {str(e)}')
+                return redirect(reverse('contato'))
+    return render(request, 'core/contato.html')
+
