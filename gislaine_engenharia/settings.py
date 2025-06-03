fef9cf370f917f2b3d5ca6a66f0c35f3e46ad0ee
Vanessa-teles <soymichefa001@gmail.com>
2025-06-03 12:37:18 -0300

    18

--- gislaine_engenharia/settings.py
+++ gislaine_engenharia/settings.py
@@ -31,12 +31,12 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',  # Este é o middleware CSRF
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
 
 ROOT_URLCONF = 'gislaine_engenharia.urls'
 
