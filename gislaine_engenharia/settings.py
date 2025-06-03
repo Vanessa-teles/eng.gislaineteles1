2dc885643fa9e1e42b1084703b383d8ec068b4e1
Vanessa-teles <soymichefa001@gmail.com>
2025-06-03 14:00:00 -0300

    1234

--- gislaine_engenharia/settings.py
+++ gislaine_engenharia/settings.py
@@ -98,9 +98,6 @@ STATICFILES_DIRS = [
 ]
 
 # Media files
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
