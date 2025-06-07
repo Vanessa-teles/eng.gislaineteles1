795950e83febc6581cbd35b9933ef672b45d52f9
Vanessa-teles <soymichefa001@gmail.com>
2025-06-07 12:02:14 -0300

    coisasd

--- gislaine_engenharia/settings.py
+++ gislaine_engenharia/settings.py
@@ -12,10 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gislaine-teles-engenharia-diagnostica-2025'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = False
 
 
-ALLOWED_HOSTS = ['*',]
+ALLOWED_HOSTS = ['31.97.31.178',]
 
 # Application definition
 INSTALLED_APPS = [
