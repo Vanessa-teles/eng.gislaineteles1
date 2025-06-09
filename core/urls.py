437f0dab09f9e10d0cff9f5de54ab8931a550fe4
Vanessa-teles <soymichefa001@gmail.com>
2025-06-09 10:10:23 -0300

    slasla2

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('contato/', views.contato, name='contato'),
+    path('favicon.jpg', RedirectView.as_view(url=staticfiles_storage.url('images/favicon.jpg'))),
 ]
